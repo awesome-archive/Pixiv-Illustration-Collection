286ec8210b70014cdee6f0a6cad6f22aec8ab7a5
39282 <392822872@qq.com>
2019-03-29 14:02:07 +0800

    v2.0

--- src/main/java/com/pixivic/model/illust/MetaPage.java
+++
@@ -1,8 +0,0 @@
-package com.pixivic.model.illust;
-
-import lombok.Data;
-
-@Data
-public class MetaPage {
-    private ImageUrls image_urls;
-}
--- src/main/java/com/pixivic/model/illust/MetaSinglePage.java
+++
@@ -1,8 +0,0 @@
-package com.pixivic.model.illust;
-
-import lombok.Data;
-
-@Data
-public class MetaSinglePage {
-    String original_image_url;
-}
