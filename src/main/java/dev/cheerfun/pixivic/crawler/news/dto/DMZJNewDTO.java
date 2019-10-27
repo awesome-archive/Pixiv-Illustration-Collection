00e640b6fb3bd22c341b943378185eed6467063e
OysterQAQ <oysterqaq@gmail.com>
2019-10-27 12:50:19 +0800

    feature: add api doc
    新增接口文档

--- src/main/java/dev/cheerfun/pixivic/crawler/news/dto/DMZJNewDTO.java
+++ src/main/java/dev/cheerfun/pixivic/crawler/news/dto/DMZJNewDTO.java
@@ -1,13 +1,11 @@
 package dev.cheerfun.pixivic.crawler.news.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import dev.cheerfun.pixivic.common.model.ACGNew;
 import lombok.Data;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
 
 /**
  * @author OysterQAQ
@@ -20,7 +18,7 @@ public class DMZJNewDTO {
     private String title;
     private String intro;
     @JsonSetter("create_time")
-    private int createTime;
+    private long createTime;
     @JsonSetter("row_pic_url")
     private String cover;
     @JsonSetter("page_url")
@@ -28,6 +26,6 @@ public class DMZJNewDTO {
     @JsonSetter("nickname")
     private String author;
     public ACGNew cast(){
-       return new ACGNew(title, intro, author, cover, refererUrl, Instant.ofEpochMilli(createTime).atZone(ZoneOffset.ofHours(8)).toLocalDate(),"动漫之家");
+       return new ACGNew(title, intro, author, cover, refererUrl, Instant.ofEpochSecond(createTime).atZone(ZoneId.systemDefault()).toLocalDate(),"动漫之家");
     }
 }
